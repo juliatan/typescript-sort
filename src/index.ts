d525e68353b3124bcd352c7ef2452c7becb60941
Julia Tan <juliajtan@gmail.com>
2020-10-29 19:15:42 +0000

    Add Sorter class that takes array of numbers and bubble sorts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-console.log('hi');
-console.log('123');
+class Sorter {
+
+  
+  // collection: number[];
+  
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+  
+  // same as this short hand notation
+  constructor(public collection: number[]) {}
+
+  // implementation of bubble sort
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1] {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j+1];
+          this.collection[j+1] = leftHand;
+        })
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
