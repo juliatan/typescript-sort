9ef6f099f61784473a1dcf04c139b540810067fc
Julia Tan <julia@joinzoe.com>
2020-11-07 18:39:45 +0000

    Update Sorter by adding specific Sortable interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,8 +1,15 @@
-import { NumbersCollection } from "./NumbersCollection";
+// Define an interface that gives instructions on what you need to have to be eligible for sorting
+// (not just for Numbers Collection)
+interface Sortable {
+  length: number;
+  // note leftIndex and rightIndex names don't have to match with say what we use in NumbersCollection
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 
 // extract out the sorting logic and don't have it working directly with solely an array of numbers
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
