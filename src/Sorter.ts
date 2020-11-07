2e3b3bcdbc5af26e4dbd2ffe7df7fc064660364f
Julia Tan <julia@joinzoe.com>
2020-11-07 22:03:28 +0000

    We don't need the Sortable interface anymore since we've changed Sorter to an abstract class

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,12 +1,3 @@
-// Define an interface that gives instructions on what you need to have to be eligible for sorting
-// (not just for Numbers Collection)
-interface Sortable {
-  length: number;
-  // note leftIndex and rightIndex names don't have to match with say what we use in NumbersCollection
-  compare(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
-}
-
 export abstract class Sorter {
   // tell Typescript that these methods don't exist in this class, but DEFINITELY will exist in child classes
   abstract compare(leftIndex: number, rightIndex: number): boolean;
