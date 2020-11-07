5952c3369715e63350110d65b8aabec4e57cfeaf
Julia Tan <julia@joinzoe.com>
2020-11-07 18:54:47 +0000

    Add ability to sort a string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([1000, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaayb");
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
