45dabc039de6c7a511dce7e0d47b6d5512f4fd16
Julia Tan <julia@joinzoe.com>
2020-11-07 21:23:49 +0000

    Add ability to sort linked list

---
+++ src/LinkedList.ts
@@ -0,0 +1,93 @@
+class Node {
+  // if next is called, it returns a Node or nothing (if at end of LinkedList). Default is null.
+  next: Node | null = null;
+
+  constructor(public data: number) {}
+}
+
+// only to store numbers
+export class LinkedList {
+  // start of LinkedList can either be a Node or nothing
+  head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error("Index out of bounds");
+    }
+
+    let counter = 0;
+    // need to specifically define node because it otherwise thinks node can only be a Node based on this.head
+    // because node.next can be null, we need to explicitly define it
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error("Index out of bounds");
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([1000, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,14 @@ const charactersCollection = new CharactersCollection("Xaayb");
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
+// console.log(linkedList.print())
